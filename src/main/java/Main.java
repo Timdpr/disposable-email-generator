851efad0820e5b6a09858d7bf72254d5ba81db51
timdpr <qotimsa@live.co.uk>
2017-05-20 11:20:03 +0100

    Added basic tests for now

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,15 +4,18 @@ import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) {
-		RandomNamesAndNouns test = new RandomNamesAndNouns();
-		try {
-			test.testFiles();
-			System.out.println(test.getFirstName());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
-
+    public static void main(String[] args) {
+        RandomNamesAndNouns test = new RandomNamesAndNouns();
+        try {
+            test.testFiles();
+            System.out.println();
+            System.out.println(test.getFirstName());
+            System.out.println(test.getLastName());
+            System.out.println(test.getNoun());
+            System.out.println();
+            test.testFiles();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
