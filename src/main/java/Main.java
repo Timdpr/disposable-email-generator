d442d4b76b81a4f9b33747f86827dfdc5c8bd037
timdpr <qotimsa@live.co.uk>
2017-05-21 15:55:41 +0100

    Proper logic for now

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,10 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-        RandomNamesAndNouns test = new RandomNamesAndNouns();
+        EmailGenerator emailGenerator = new EmailGenerator();
         try {
-            test.testFiles();
-            System.out.println();
-            System.out.println(test.getFirstName());
-            System.out.println(test.getLastName());
-            System.out.println(test.getNoun());
-            System.out.println();
-            test.testFiles();
+            emailGenerator.start(5);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
