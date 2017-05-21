53730de2cf3591fb9d3295ac2a715f6b9e869149
timdpr <qotimsa@live.co.uk>
2017-05-21 15:56:18 +0100

    Implemented EmailGenerator functionality

--- src/main/java/EmailGenerator.java
+++ src/main/java/EmailGenerator.java
@@ -1,5 +1,40 @@
 package main.java;
 
+import java.io.IOException;
+import java.util.Random;
+
 public class EmailGenerator {
 
+    private RandomEmailParts rP = new RandomEmailParts();
+    private Random random = new Random();
+
+    public void start(int howMany) throws IOException{
+        while (howMany > 0) {
+            switch (getCombo()) {
+            case 1: System.out.println(rP.getFirstName() +rP.getLastName()+ rP.getEmail());
+                    break;
+            case 2: System.out.println(rP.getFirstName() + rP.getNoun() + rP.getEmail());
+                    break;
+            case 3: System.out.println(rP.getNoun() + rP.getFirstName() + rP.getEmail());
+                    break;
+            case 4: System.out.println(rP.getFirstName() + rP.getLastName()
+                                     + random.nextInt(100) + rP.getEmail());
+                    break;
+            }
+            howMany--;
+        }
+    }
+
+    public int getCombo() {
+        double d = random.nextDouble();
+        if (d < 0.5) {
+            return 1;
+        } else if (d < 0.65) {
+            return 2;
+        } else if (d < 0.85) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
 }
