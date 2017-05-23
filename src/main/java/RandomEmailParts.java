7febd8f09e7053debf3a28055b53eb71009c6a56
timdpr <qotimsa@live.co.uk>
2017-05-23 11:30:42 +0100

    Implemented rejection sampling

--- src/main/java/RandomEmailParts.java
+++ src/main/java/RandomEmailParts.java
@@ -31,7 +31,7 @@ public class RandomEmailParts {
     public String getLastName() throws IOException {
         return getRandomLineText(this.lastNames);
     }
-    
+
     public String getEmail() throws IOException {
         return getRandomLineText(this.emailList);
     }
@@ -43,22 +43,41 @@ public class RandomEmailParts {
     /**
      * @return String of random line in parameter file
      * 
+     * Line is chosen using rejection sampling:
      * Seeks backwards from random byte to find '\n' char; reads following line
-     * Biased towards longer lines - should modify to use rejection sampling
+     * Returns line with probability min line length (2) / line length
+     * (2 is incorrect for email domains, but works well enough!)
      */
     public String getRandomLineText(RandomAccessFile f) throws IOException {
-        long randomByteLocation = (long) (Math.random() * f.length());
+        long randomByteLocation = 1;
+        while (randomByteLocation > 0) {
+            randomByteLocation = getRandomByteLocation(f);
 
-        while (randomByteLocation >= 0) {
-            f.seek(randomByteLocation);   // Move pointer to location
+            f.seek(randomByteLocation); // Move pointer to location
             char c = (char) f.readByte(); // Get byte, cast to char
 
-            if (c == '\n') {
-                return f.readLine();      // If newline char, return following line
+            if (c == '\n') { // Move to start of line
+                String line = f.readLine();
+                // Return line with probability of min line length / line length
+                double probability = 2.0 / line.length();
+                if (Math.random() < probability) {
+                    return line;
+                }
             } else {
-                randomByteLocation --;    // Else try again with previous char
+                randomByteLocation--; // Else try again with previous char
             }
         }
         return "getRandomLineText failed";
+    }
+
+    /**
+     * @return Location of random byte in parameter file, except 0
+     */
+    public long getRandomByteLocation(RandomAccessFile f) throws IOException {
+        long randomByteLocation = (long) (Math.random() * f.length());
+        if (randomByteLocation == 0) {
+            randomByteLocation++;
+        }
+        return randomByteLocation;
     }
 }
